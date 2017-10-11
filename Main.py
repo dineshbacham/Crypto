b8277bb55ba868b1fc29df5618ce95c3f43e0929
Dinesh Bacham <dinesh123reddy@gmail.com>
2017-10-11 19:14:36 -0400

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,3 +1,12 @@
+Load Libraries
+
+import pandas as pd
+from bittrex import Bittrex
+
+my_bittrex = Bittrex("99f9f179b8e84a43a1204903a5c242eb", "0994a3ebc9aa4d0fa44d0c9d94d272bc")
+my_bittrex.get_markets()
+# Key = 99f9f179b8e84a43a1204903a5c242eb
+# Secret = 0994a3ebc9aa4d0fa44d0c9d94d272bc
 # Function to extract required data from the bittrex API - Symbol, time frame, frequency
 
 
@@ -12,5 +21,3 @@
 
 # Momentum
 
-
-#Testing new changes
